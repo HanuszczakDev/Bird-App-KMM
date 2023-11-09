3a7be6909a760a5b734d8fab3004d9e6d0e04564
ahanuszczak <adam.hanuszczak@gmail.com>
2023-11-09 19:55:21 +0100

    added Categories buttons

--- shared/src/commonMain/kotlin/BirdsViewModel.kt
+++ shared/src/commonMain/kotlin/BirdsViewModel.kt
@@ -11,8 +11,12 @@ import kotlinx.coroutines.launch
 import model.BirdImage
 
 data class BirdUiState(
-    val images: List<BirdImage> = emptyList()
-)
+    val images: List<BirdImage> = emptyList(),
+    val selectedCategory: String? = null
+) {
+    val categories = images.map { it.category }.toSet()
+    val selectedImages = images.filter { it.category == selectedCategory }
+}
 class BirdsViewModel: ViewModel() {
     private val _uiState = MutableStateFlow<BirdUiState>(BirdUiState())
     val uiState = _uiState.asStateFlow()
@@ -29,6 +33,12 @@ class BirdsViewModel: ViewModel() {
 
     override fun onCleared() {
         httpClient.close()
+    }
+
+    fun selectedCategory(category: String) {
+        _uiState.update {
+            it.copy(selectedCategory = category)
+        }
     }
 
     private fun updateImages() {
